f760dd1f34bfaeb59cc79ba93e6ed737366f9c08
Mark Ng'ang'a <maqnganga@gmail.com>
2017-04-29 01:45:56 +0300

    Fix 2015 special case for gazette volume (VoI =/= Vol)

--- opengazettes/spiders/gazettes.py
+++ opengazettes/spiders/gazettes.py
@@ -56,8 +56,9 @@ class GazettesSpider(scrapy.Spider):
                 if not previous_volume_number:
                     gazette_meta['gazette_volume'] = romanify.roman2arabic(
                         row.xpath('td/a/@href')
-                        .re(r'Vol*.*No')[0]
-                        .replace('Vol', '').replace('.', '').replace('-', '')
+                        .re(r'(Vol*.*No)|(VoI*.*No)')[0]
+                        .replace('Vol', '').replace('VoI', '')
+                        .replace('.', '').replace('-', '')
                         .replace(' ', '').replace('l', 'I').replace('No', ''))
 
                     previous_volume_number = gazette_meta['gazette_volume']
