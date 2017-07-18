d5d8e11665df98f7f9eb06c30a6d5973f6798db6
Ahmedsebit <Ahmed.yusuf@andela.com>
2017-07-18 14:59:43 +0300

    ADD slack noification for failling scrappers

--- opengazettes/spiders/gazettes.py
+++ opengazettes/spiders/gazettes.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
+
 from datetime import datetime
 from ..items import OpengazettesItem
 import romanify
+from scrapy import signals
+import requests
+import json
 
 
 class GazettesSpider(scrapy.Spider):
@@ -17,11 +25,60 @@ class GazettesSpider(scrapy.Spider):
             year = self.year
         except AttributeError:
             year = datetime.now().strftime('%Y')
-
+        
         url = 'http://kenyalaw.org/kenya_gazette/gazette/year/%s' % \
             (year)
-        yield scrapy.Request(url, callback=self.parse)
+        yield scrapy.Request(url, callback=self.parse, errback=self.errback, dont_filter=True)
 
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(GazettesSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.item_dropped, signal=signals.item_dropped)
+        return spider
+
+    def item_dropped(self, item, response, exception, spider):
+        spider.logger.info('Spider opened: %s', spider.name)
+        webhook_url = 'https://hooks.slack.com/services/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'
+        message = item +" Item has been dropped"
+        slack_data = {
+                    "attachments":
+                        [
+                            {
+                                "author_name": "Code4Africa",
+                                "author_icon": "https://codeforafrica.org/img/logos/c4a.png",
+                                "color": "danger",
+                                "pretext": "[ERROR] New Alert for failing scrapers",
+                                "text": message,
+                                "fields": [
+                                    {
+                                        "title": "Spider",
+                                        "value": spider.name,
+                                        "short": False
+                                        },
+                                        {
+                                        "title": "Failed Item",
+                                        "value": item,
+                                        "short": False
+                                        },
+                                        {
+                                        "title": "Response",
+                                        "value": response,
+                                        "short": False
+                                        },
+                                        {
+                                        "title": "Exception",
+                                        "value": exception,
+                                        "short": False
+                                        }
+                                    ],
+                                "image_url": ":warning:",
+                                "footer": "Slack API",
+                                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                                }
+                            ]
+                    }
+
+    
     def parse(self, response):
         # Get all rows in the "Weekly Issues" div
         weekly_rows = response.xpath('//*[@id="content"]/div[1]/table/tr')
@@ -77,6 +134,106 @@ class GazettesSpider(scrapy.Spider):
                                          callback=self.open_single_gazette)
                 request.meta['gazette_meta'] = gazette_meta
                 yield request
+        
+
+    def errback(self, failure):
+        self.logger.error(repr(failure))
+        webhook_url = 'https://hooks.slack.com/services/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'
+        if failure.check(HttpError):
+            # these exceptions come from HttpError spider middleware
+            # you can get the non-200 response
+            message = "Exceptions from HttpError spider middleware due to non-200 response"
+            slack_data = {
+                        "attachments":
+                            [
+                                {
+                                    "author_name": "Code4Africa",
+                                    "author_icon": "https://codeforafrica.org/img/logos/c4a.png",
+                                    "color": "danger",
+                                    "pretext": "[ERROR] New Alert for failing scrapers",
+                                    "fields": [
+                                        {
+                                            "title": "HttpError",
+                                            "value": message,
+                                            "short": False
+                                            }
+                                        ],
+                                    "image_url": ":warning:",
+                                    "footer": "Slack API",
+                                    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                                    }
+                                ]
+                        }
+            response = requests.post(
+                webhook_url, data=json.dumps(slack_data),
+                headers={'Content-Type': 'application/json'}
+            )
+
+            response = failure.value.response
+            self.logger.error('HttpError on %s', response.url)
+
+        elif failure.check(DNSLookupError):
+            # these exceptions come from DNSLookupError spider middleware
+            message = "Exceptions from DNSLookupError spider middleware"
+            slack_data = {
+                        "attachments":
+                            [
+                                {
+                                    "author_name": "Code4Africa",
+                                    "author_icon": "https://codeforafrica.org/img/logos/c4a.png",
+                                    "color": "danger",
+                                    "pretext": "[ERROR] New Alert for failing scrapers",
+                                    "fields": [
+                                        {
+                                            "title": "DNSLookupError",
+                                            "value": message,
+                                            "short": False
+                                            }
+                                        ],
+                                    "image_url": ":warning:",
+                                    "footer": "Slack API",
+                                    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                                    }
+                                ]
+                        }
+            response = requests.post(
+                webhook_url, data=json.dumps(slack_data),
+                headers={'Content-Type': 'application/json'}
+            )
+            request = failure.request
+            self.logger.error('DNSLookupError on %s', request.url)
+
+        elif failure.check(TimeoutError, TCPTimedOutError):
+            # these exceptions come from TCPTimeOutError spider middleware
+            message = "Exceptions from TCPTimedOutError spider middleware"
+            slack_data = {
+                        "attachments":
+                            [
+                                {
+                                    "author_name": "Code4Africa",
+                                    "author_icon": "https://codeforafrica.org/img/logos/c4a.png",
+                                    "color": "danger",
+                                    "pretext": "[ERROR] New Alert for failing scrapers",
+                                    "fields": [
+                                        {
+                                            "title": "TimeOutError",
+                                            "value": message,
+                                            "short": False
+                                            }
+                                        ],
+                                    "image_url": ":warning:",
+                                    "footer": "Slack API",
+                                    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                                    }
+                                ]
+                        }
+            response = requests.post(
+                webhook_url, data=json.dumps(slack_data),
+                headers={'Content-Type': 'application/json'}
+            )
+            request = failure.request
+            self.logger.error('TimeoutError on %s', request.url)
+
 
     # Visit individual gazettes link
     # Find PDF download link
@@ -112,3 +269,4 @@ class GazettesSpider(scrapy.Spider):
         # Set file URLs to be downloaded by the files pipeline
         item['file_urls'] = [item['download_link']]
         yield item
+    
